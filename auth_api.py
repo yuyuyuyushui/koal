a24ae6550b4d7b28a9fcd7cedec0a488ad41f547
e <786313105@qq.com>
2020-03-27 00:11:37 +0800

    2020-03-27

--- auth_api.py
+++ auth_api.py
@@ -26,16 +26,19 @@ if __name__=="__main__":
     import hashlib, json, base64
     from Crypto.Cipher import AES
     key = 'dffc2b944d5665075a0bd81657c620a0'
-
+    BLOCK_SIZE = 16  # Bytes
+    pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * \
+                    chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
     appTag = '2222222222'
     nodeId = "333"
     profileTag = '111111'
     valeu = 'dffc2b944d5665075a0bd81657c620a0'
+    unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
     response = Config("https://10.11.220.162").get_key(appTag, nodeId, profileTag)
     random = response.response["data"]["random"]
-    print(random, type(random))
-
+    q = '5a850ccb07fa267a'
+    # print(random, type(random))
     def add_to_16(value):
         while len(value) % 32 != 0:
             value += '\0'
@@ -43,13 +46,14 @@ if __name__=="__main__":
 
 
     # t = base64.b64decode(random)
-    aes = AES.new(key[0:16].encode("utf-8"),  AES.MODE_ECB)  # 初始化加密器，本例采用ECB加密模式
-
+    aes = AES.new(key.encode("utf-8"),  AES.MODE_ECB)  # 初始化加密器，本例采用ECB加密模式
+    # b = aes.encrypt(valeu.encode("utf-8"))
     # aes.encrypt(random)
     # random.encode("utf-8")
 
     a = aes.decrypt(bytes(random, encoding="utf-8"))
-    print(a,type(a))
+    b = str(base64.b64encode(a), encoding="utf8")
+    print(b, unpad(b), type(b))
 
     # date = {
     #     "response": hashlib.md5(aes.encode("utf-8")).hexdigest()
--- test.py
+++ test.py
@@ -1,42 +1,40 @@
-import binascii
-import re
+import base64
 from Crypto.Cipher import AES
 
 
-class AESCBC:
-    def __init__(self):
-        self.key = '0123456789012345'  # 定义key值
-        self.mode = AES.MODE_CBC
-        self.bs = 16  # block size
-        self.PADDING = lambda s: s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+class EncryptDate:
+    def __init__(self, key):
+        self.key = key  # 初始化密钥
+        self.length = AES.block_size  # 初始化数据块大小
+        self.aes = AES.new(self.key.encode("utf-8"), AES.MODE_ECB)  # 初始化AES,ECB模式的实例
+        # 截断函数，去除填充的字符
+        self.unpad = lambda date: date[0:-ord(date[-1])]
 
-    def encrypt(self, text):
-        generator = AES.new(self.key, self.mode)  # 这里的key 和IV 一样 ，可以按照自己的值定义
-        crypt = generator.encrypt(self.PADDING(text))
-        # crypted_str = base64.b64encode(crypt)   #输出Base64
-        crypted_str = binascii.b2a_hex(crypt)  # 输出Hex
-        result = crypted_str.decode()
-        return result
+    def pad(self, text):
+        """
+        #填充函数，使被加密数据的字节码长度是block_size的整数倍
+        """
+        count = len(text.encode('utf-8'))
+        add = self.length - (count % self.length)
+        entext = text + (chr(add) * add)
+        return entext
 
-    def decrypt(self, text):
-        generator = AES.new(self.key, self.mode)
-        text += (len(text) % 4) * '='
-        # decrpyt_bytes = base64.b64decode(text)           #输出Base64
-        decrpyt_bytes = binascii.a2b_hex(text)  # 输出Hex
-        meg = generator.decrypt(decrpyt_bytes)
-        # 去除解码后的非法字符
-        try:
-            result = re.compile('[\\x00-\\x08\\x0b-\\x0c\\x0e-\\x1f\n\r\t]').sub('', meg.decode())
-        except Exception:
-            result = '解码失败，请重试!'
-        return result
+    def encrypt(self, encrData):  # 加密函数
+        res = self.aes.encrypt(self.pad(encrData).encode("utf8"))
+        msg = str(base64.b64encode(res), encoding="utf8")
+        return msg
+
+    def decrypt(self, decrData):  # 解密函数
+
+        res = base64.decodebytes(decrData.encode("utf8"))
+        print(res)
+        print(str(base64.b64encode(res), encoding="utf8"))
+        msg = self.aes.decrypt(decrData.encode("utf-8"))
+        m = str(base64.b64encode(msg), encoding="utf8")
+        return self.unpad(m)
 
 
-if __name__ == '__main__':
-    aes = AESCBC()
-
-    to_encrypt = '123456'
-    to_decrypt = '31f45bc15c28b3dffb7886d86ae0136d'
-
-    print("\n加密前:{0}\n加密后：{1}\n".format(to_encrypt, aes.encrypt(to_encrypt)))
-    print("解密前:{0}\n解密后：{1}".format(to_decrypt, aes.decrypt(to_decrypt)))
+eg = EncryptDate("dffc2b944d5665075a0bd81657c620a0")  # 这里密钥的长度必须是16的倍数
+res1 = eg.encrypt("qqq111188888888888888")
+print(res1)
+# print(eg.decrypt('c298c414e14c6a5aea82a236f796a7269e2cdbc7c87f4a497fac52d42d483484'))
