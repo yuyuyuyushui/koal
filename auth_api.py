3db86ef30a30bf5c120969b698a7dca75a6ee3da
e <786313105@qq.com>
2020-03-30 00:00:58 +0800

    2020-03-30

--- auth_api.py
+++ auth_api.py
@@ -1,9 +1,9 @@
-from core.rest_client import *
+from core.rest_client2 import *
 
 
-class Config(RestClient):
-    def __init__(self, api_url_path, **kwargs):
-        super(Config, self).__init__(api_url_path, **kwargs)
+class Config_first(RestClient2):
+    def __init__(self,**kwargs):
+        super(Config_first, self).__init__(**kwargs)
 
     def get_key(self, appTag, nodeId, profileTag, **kwargs):
         return self.get("/v1/api/config/{}/{}/{}/hello".format(appTag, nodeId, profileTag), **kwargs)
@@ -13,51 +13,54 @@ class Config(RestClient):
         print(self.session.headers)
         return self.post("/v1/api/config/{}/{}/{}/hello".format(appTag,nodeId,profileTag),**kwargs)
 
-    def Authentication(self,token,appTag,nodeId,profileTag):
-        self.session.headers["token"] = token
 
-        return self.post("/v1/api/config/{}/{}/{}/all".format(appTag,nodeId,profileTag))
+class Config_seconde(RestClient1):
+    def __init__(self,**kwargs):
+        super(Config_seconde, self).__init__(**kwargs)
 
-    def query_config(self,token,appTag,nodeId,profileTag):
-        self.session.headers["token"] = token
+    def Authentication(self,appTag,nodeId,profileTag):
+        print(self.session.headers)
+        return self.get("/v1/api/config/{}/{}/{}/all".format(appTag,nodeId,profileTag))
 
+    def query_config(self,appTag,nodeId,profileTag):
         return self.get("/v1/api/config/{}/{}/{}/update".format(appTag,nodeId,profileTag))
 if __name__=="__main__":
     import hashlib, json, base64
     from Crypto.Cipher import AES
     key = 'dffc2b944d5665075a0bd81657c620a0'
-    BLOCK_SIZE = 16  # Bytes
-    pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * \
-                    chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+    url = 'https://10.11.220.162'
     appTag = '2222222222'
     nodeId = "333"
     profileTag = '111111'
-    valeu = 'dffc2b944d5665075a0bd81657c620a0'
+
     unpad = lambda s: s[:-ord(s[len(s) - 1:])]
-
-    response = Config("https://10.11.220.162").get_key(appTag, nodeId, profileTag)
-    random = response.response["data"]["random"]
-    q = '5a850ccb07fa267a'
-    # print(random, type(random))
-    def add_to_16(value):
-        while len(value) % 32 != 0:
-            value += '\0'
-        return str.encode(value)
-
-
-    # t = base64.b64decode(random)
+    # 向服务器第一次发起请求，获取随机AES加密值
+    response1 = Config_first(api_url_path=url).get_key(appTag, nodeId, profileTag)
+    random = response1.response["data"]["random"]
+    # 获取响应数据AES加密值，对响应数据进行hex解码
+    b_random = bytes.fromhex(random)
+    print(b_random,type(b_random))
+    # 初始化加密器，ECB加密
     aes = AES.new(key.encode("utf-8"),  AES.MODE_ECB)  # 初始化加密器，本例采用ECB加密模式
-    # b = aes.encrypt(valeu.encode("utf-8"))
-    # aes.encrypt(random)
-    # random.encode("utf-8")
+    c_random = unpad(aes.decrypt(b_random))
+    # 对AES解码进行PKCS5Padding取值
+    print(c_random, type(c_random))
 
-    a = aes.decrypt(bytes(random, encoding="utf-8"))
-    b = str(base64.b64encode(a), encoding="utf8")
-    print(b, unpad(b), type(b))
-
-    # date = {
-    #     "response": hashlib.md5(aes.encode("utf-8")).hexdigest()
-    # }
-    # print(type(date["response"]), date["response"])
-    # response=Config("https://10.11.220.162").get_token(appTag, nodeId, profileTag, json=date)
-    # print(response.response)
+    # 第二步，对响应值先进行md5加密，再向服务器请求token
+    date = {
+                "response": hashlib.md5(c_random).hexdigest()
+            }
+    print(type(date["response"]), date["response"])
+    response2=Config_first(api_url_path=url).get_token(appTag, nodeId, profileTag, json=date)
+    token = response2.response["data"]["token"]
+    print(response2.response)
+    # 第二阶段 获取配置信息
+    response3 = Config_seconde(api_url_path=url,token=token).Authentication(appTag, nodeId, profileTag)
+    print(response3.response)
+    configData = response3.response["data"][0]["configData"]
+    b_conifgDate = bytes.fromhex(configData)
+    c_configdata = unpad(aes.decrypt(b_conifgDate))
+    print(c_configdata.decode())
+    # 第四阶段 查询配置更新
+    response4 = Config_seconde(api_url_path=url,token=token).query_config(appTag, nodeId, profileTag)
+    print(response4.response)
--- core/rest_client2.py
+++ core/rest_client2.py
@@ -1,7 +1,7 @@
 import requests
 from core.base import *
 
-class RestClient():
+class RestClient1():
     def __init__(self, api_url_path, username=None,password=None,token=None,**kwargs):
         self.session = requests.session()
 
@@ -31,14 +31,33 @@ class RestClient():
     def patch(self,url,**kwargs):
         self.url = self.api_url_path +url
         return self.session.request('patch', self.url, verify = False,**kwargs)
-
+class RestClient2():
+    # def __init__(self, api_url_path, username=None,password=None,token=None,**kwargs):
+    #     self.session = requests.session()
+    #
+    #     if username and password:
+    #         self.session.auth = (username, password)
+    #     elif token:
+    #         self.session.headers['token']=token
+    #
+    #     else:
+    #          print('输入有误')
+    #     self.api_url_path = api_url_path
+    #     self.url = None
+    def __init__(self,api_url_path,**kwargs):
+        self.session = requests.session()
+        self.api_url_path = api_url_path
     @response
-    def put(self, url, **kwargs):
-        self.url = self.api_url_path+url
-        return self.session.request('put', self.url, verify = False, **kwargs)
-
-    @response
-    def delete(self, url, **kwargs):
+    def get(self, url, **kwargs):
         self.url = self.api_url_path + url
-        return self.session.request('delete', self.url,verify = False, **kwargs)
+        return self.session.request('get', self.url, verify = False,**kwargs)
 
+    @response
+    def post(self, url, **kwargs):
+        self.url = self.api_url_path + url
+        return self.session.request('post', self.url, verify = False, **kwargs)
+
+    @response
+    def patch(self,url,**kwargs):
+        self.url = self.api_url_path +url
+        return self.session.request('patch', self.url, verify = False,**kwargs)
