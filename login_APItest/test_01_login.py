a34f70a254edc5bbbed4a8a61e87de1070ce0fd2
luozhiqiang <786313105@qq.com>
2020-06-10 17:43:23 +0800

    Merge branch 'master' of https://github.com/yuyuyuyushui/koal
    # Conflicts:
    #	api_test/conftest.py
    #	api_test/test_03_role.py
    #	core/rest_client.py

--- login_APItest/test_01_login.py
+++ login_APItest/test_01_login.py
@@ -1,9 +1,12 @@
 from Login import *
 import pytest
 login_data=[
-    ('ghcatest','1111111','5','用户帐号被禁用'),
-    ('ghcatest','1111112','5',"用户帐号被禁用"),
-    ('ghcatest','1111113','5','用户帐号被禁用')
+    ('ghcatest','1111111','5','账号或密码有误'),
+    # ('ghcatest','1111112','5',"账号或密码有误"),
+    # ('ls','1111113','5','账号或密码有误'),
+    # ('ls','1111111','5','账号或密码有误'),
+    # ('jack','1111111','5','账号或密码有误'),
+    # ('jack','1111111','5','连续登录失败,IP被禁用')
 ]
 @pytest.mark.parametrize("loginname,password,verifyType,expect",login_data)
 def test_login_false(loginname,password,verifyType,expect):
