1316fedea6db093b4398f7b1901154d654b0d3b5
luozhiqiang <786313105@qq.com>
2020-08-05 17:45:35 +0800

    Merge branch 'master' of https://github.com/yuyuyuyushui/koal
    # Conflicts:
    #	api_test/conftest.py
    #	api_test/test_03_role.py
    #	core/rest_client.py

--- api/operation_portal/portal.py
+++ api/operation_portal/portal.py
@@ -35,3 +35,55 @@ class Portal(RestClient):
 
     def mysql_agreement_agency_session_start_up(self, **kwargs):
         return self.post("/v1/portal/session/submitdb", **kwargs)
+
+    def resource_access_submit(self, **kwargs):
+        return self.put("/v1/portal/session/submit",**kwargs)
+
+    def web_terminal_character_sign_in(self,**kwargs):
+        return self.get("/v1/koal/connect", **kwargs)
+
+    def web_terminal_rdp_sign_in(self, **kwargs):
+        return self.get("character", **kwargs)
+
+    def resource_access_shortcut_key_Collection(self,**kwargs):
+        return self.put("/v1/portal/favorites/detail/add",**kwargs)
+
+    def shortcut_favorite_list(self, **kwargs):
+        return self.get("/v1/portal/favorites/list", **kwargs)
+
+    def shortcut_favorite_detailed_list(self, **kwargs):
+        return self.get("/v1/portal/favorites/detail/list",**kwargs)
+
+    def add_shortcut_visit_favorite(self, **kwargs):
+        return self.post("/v1/portal/favorites/add", **kwargs)
+
+    def update_shortcut_visit_favorite(self, **kwargs):
+        return self.put("/v1/portal/favorites/update",**kwargs)
+
+    def delete_shortcut_visit_favorite(self, favoritesId,**kwargs):
+        return self.delete("/v1/portal/favorites/remove/{}".format(favoritesId),**kwargs)
+
+    def delet_shortcut_collection_history(self,id,**kwargs):
+        return self.delete("/v1/portal/favorites/item/remove/{}".format(id),**kwargs)
+
+    def temporary_grant_authorization_apply_history(self, **kwargs):
+        return self.get("/v1/portal/apply/list",**kwargs)
+
+    def add_temporary_grant_authorization_apply(self, **kwargs):
+        return self.post("/v1/portal/apply/add", **kwargs)
+
+    def grant_authorization_temporary_apply_choice_applied_asserts_list(self, **kwargs):
+        return self.get("/v1/portal/apply/res/list",**kwargs)
+
+    def grant_authorization_temporary_apply_bussiness_list(self,**kwargs):
+        return self.get("/v1/portal/apply/abis/list", **kwargs)
+
+    def query_temporary_grant_authorization_applied_history_detail(self, **kwargs):
+        return self.get("/v1/portal/apply/view/{applyId}", **kwargs)
+
+    def modefy_temporary_grant_authorization_apply(self, **kwargs):
+        return self.put("/v1/portal/apply/update", **kwargs)
+
+    def delete_temporary_grant_authorization_apply_history(self,applyId,**kwargs):
+        return self.delete("/v1/portal/apply/remove/{}".format(applyId),**kwargs)
+
